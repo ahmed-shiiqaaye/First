1b32b6565088300b79e45b8d40a19852d26af601
ahmed-shiiqaaye <me.shiqaye@gmail.com>
2022-03-21 19:32:24 +0200

    wow

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let menu =document.getElementById('menu')
 let links = document.querySelector('.links')
 
 menu.addEventListener('click',function(){
-    links.classList.add('active')
+    links.classList.toggle('active')
 })
 times.addEventListener('click',function(){
     links.classList.remove('active')
